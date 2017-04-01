678a71a081688e9aca0f8af712ac75a3c29ea98a
wangzexin <wang.zexin@dhs.sg>
2017-04-01 19:24:06 +0800

    [Report] Derive closed form formula for european call and put

--- Implementation/Closed-form formula/closedFormFormula.py
+++ Implementation/Closed-form formula/closedFormFormula.py
@@ -7,3 +7,10 @@ def europeanCall(St, K, h, r, sigma):
     N1 = scs.norm.cdf(d1)
     N2 = scs.norm.cdf(d2)
     return St * N1 - K * np.exp(-r * h) * N2
+
+def test():
+    print(europeanCall(100, 110, 1, 0.05, 0.25))
+    print(europeanCall(100, 120, 1, 0.05, 0.25))
+    print(europeanCall(100, 130, 1, 0.05, 0.25))
+    print(europeanCall(100, 140, 1, 0.05, 0.25))
+    print(europeanCall(100, 150, 1, 0.05, 0.25))
