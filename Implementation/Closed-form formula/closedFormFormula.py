50269932d4d284f64f99ef9d27a8e97ba7f2b5e7
wangzexin <wang.zexin@dhs.sg>
2017-04-03 00:34:06 +0800

    [Implementation] Add closed form formula for Barrier down and out call option

--- Implementation/Closed-form formula/closedFormFormula.py
+++ Implementation/Closed-form formula/closedFormFormula.py
@@ -23,3 +23,28 @@ def testPut():
     for i in range(105, 205, 5):
         print(europeanPut(100, i, 1, 0.05, 0.25))
 
+def barrierCallOption(S, K, B, h, r, sigma):
+    if B < K:
+        result1 = europeanCall(S, K, h, r, sigma)
+        result2 = (B / S) ** (2*r/(sigma**2)-1) * europeanCall(B**2/S, K, h, r, sigma)
+        return result1 - result2
+    else:
+        d5 = (np.log(S/B) + (r + sigma**2/2) * h) / (sigma * h ** 0.5)
+        d6 = d5 - sigma * h ** 0.5
+        d7 = (np.log(B/S) + (r + sigma**2/2) * h) / (sigma * h ** 0.5)
+        d8 = d7 - sigma * h ** 0.5
+        N5 = scs.norm.cdf(d5)
+        N6 = scs.norm.cdf(d6)
+        N7 = scs.norm.cdf(d7)
+        N8 = scs.norm.cdf(d8)
+        result1 = S * N5 - K * np.exp(-r * h) * N6
+        result3 = (B/S)**(2*r/(sigma**2)+1) * S * N7
+        result2 = (B/S)**(2*r/(sigma**2)-1) * K * np.exp(-r * h) * N8
+        return result1 + result2 - result3
+
+def testBarrierDownOutCall():
+    for i in range(70, 170, 10):
+        print(str(i)+"&",end="")
+        for j in range(75, 125, 10):
+            print(str(round(barrierCallOption(100, i, j, 1, 0.05, 0.25),10))+'&', end="")
+        print()
--- Implementation/Monte Carlo methods in Financial Engineering/control_variate_underlying_price.py
+++ Implementation/Monte Carlo methods in Financial Engineering/control_variate_underlying_price.py
@@ -53,3 +53,26 @@ def control_variate_underlying_assets_european_put(strikePrice):
 def testPut():
     for i in range(105, 205, 5):
         print(control_variate_underlying_assets_european_put(i))
+
+def control_variate_underlying_assets_european_put(strikePrice):
+    # parameters
+    sigma = 0.25;   T = 1.0;   I = 500000;   S0 = 100;   r = 0.05
+    # pilot simulation
+    standardNormals = npr.standard_normal(I)
+    stockPrices = S0*np.exp((r-sigma**2/2)*T+sigma*np.sqrt(T)*standardNormals)
+    optionPayoffs = np.maximum(stockPrices - strikePrice, 0)
+    covarianceMatrix = np.cov(stockPrices, optionPayoffs)
+    lambdaStar = covarianceMatrix[0][1] / covarianceMatrix[0][0]
+    # main simulation
+    standardNormals = npr.standard_normal(I)
+    stockPrices = S0*np.exp((r-sigma**2/2)*T+sigma*np.sqrt(T)*standardNormals)
+    optionPayoffs = np.maximum(strikePrice - stockPrices, 0)
+    # Replace np.exp(r*T-sigma**2/2)*S0 with np.average(stockPrices)
+    newPayoffs = optionPayoffs - lambdaStar * (stockPrices - np.average(stockPrices))
+    optionPayoffs *= np.exp(-r*T)
+    newPayoffs *= np.exp(-r*T)
+    oldEstimate = np.average(optionPayoffs)
+    oldVariance = np.var(optionPayoffs)
+    newEstimate = np.average(newPayoffs)
+    estimateVariance = np.var(newPayoffs)
+    return (oldEstimate, oldVariance, newEstimate, estimateVariance)
