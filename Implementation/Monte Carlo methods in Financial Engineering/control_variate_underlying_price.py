d4df8a4018515ccc12c60b44a3af7e30b889566b
wangzexin <wang.zexin@dhs.sg>
2017-04-04 15:42:41 +0800

    [Implementation] Correct bugs in Barrier call option pricing using Monte Carlo simulation

--- Implementation/Monte Carlo methods in Financial Engineering/control_variate_underlying_price.py
+++ Implementation/Monte Carlo methods in Financial Engineering/control_variate_underlying_price.py
@@ -54,22 +54,52 @@ def testPut():
     for i in range(105, 205, 5):
         print(control_variate_underlying_assets_european_put(i))
 
-def control_variate_underlying_assets_down_out_call(S, strikePrice, barrier, T, r, sigma):
+import math
+import random
+
+def barrier_down_out_call(S, strikePrice, barrier, T, r, sigma):
     # parameters
-    I = 5;   n = 100;    dt = T / n;     S0 = S
-    optionPayoffs = []
-    stockPrices = []
-    # pilot simulation
+    I = 1000000;   n = 1000;    dt = T / n;     S0 = S
+    barrier = barrier * math.exp(0.5826 * sigma * dt ** 0.5)
+    totalPayoff = 0
     for i in range(0, I):
-        standardNormals = list(npr.standard_normal(n))
         stockPrice = S0 + 1 - 1
         flag = True
         for j in range(0, n):
-            print(stockPrice, end = " ")
-            stockPrice = stockPrice*np.exp((r-sigma**2/2)*dt+sigma*np.sqrt(dt)*standardNormals[j])
+            z = random.normalvariate(0, 1)
+            stockPrice = stockPrice*math.exp((r-sigma**2/2)*dt+sigma*dt**0.5*z)
             if stockPrice < barrier:
                 flag = False
+                break
+        if flag:
+            optionPayoff = max(stockPrice - strikePrice, 0)
+        else:
+            optionPayoff = 0
+        totalPayoff += optionPayoff
+    return totalPayoff / I / math.exp(r * T)
+
+def testBarrierDownOutCall():
+    for i in range(70, 170, 10):
+        print(str(i)+"&",end="")
+        for j in range(75, 125, 10):
+            print(str(round(barrier_down_out_call(100, i, j, 1, 0.05, 0.25),10))+'&', end="")
         print()
+
+def control_variate_underlying_assets_down_out_call(S, strikePrice, barrier, T, r, sigma):
+    # parameters
+    I = 1000000;   n = 1000;    dt = 0.001;     S0 = S
+    barrier = barrier * math.exp(0.5826 * sigma * dt ** 0.5)
+    optionPayoffs = []
+    stockPrices = []
+    # pilot simulation
+    for i in range(0, I//100):
+        stockPrice = S0 + 1 - 1
+        flag = True
+        for j in range(0, n):
+            z = random.normalvariate(0, 1)
+            stockPrice = stockPrice*math.exp((r-sigma**2/2)*dt+sigma*dt**0.5*z)
+            if stockPrice < barrier:
+                flag = False
         if flag:
             optionPayoffs.append(max(stockPrice - strikePrice, 0))
         else:
@@ -81,28 +111,18 @@ def control_variate_underlying_assets_down_out_call(S, strikePrice, barrier, T, 
     lambdaStar = covarianceMatrix[0][1] / covarianceMatrix[0][0]
     
     # main simulation
-    optionPayoffs = []
-    stockPrices = []
+    totalPayoff = 0
+    ST = S0 * math.exp(r * T)
     for i in range(0, I):
-        standardNormals = list(npr.standard_normal(n))
         stockPrice = S0 + 1 - 1
         flag = True
         for j in range(0, n):
-            stockPrice = stockPrice*np.exp((r-sigma**2/2)*dt+sigma*np.sqrt(dt)*standardNormals[j])
+            z = random.normalvariate(0, 1)
+            stockPrice = stockPrice*math.exp((r-sigma**2/2)*dt+sigma*dt**0.5*z)
             if stockPrice < barrier:
                 flag = False
         if flag:
-            optionPayoffs.append(max(stockPrice - strikePrice, 0))
-        else:
-            optionPayoffs.append(0)
-        stockPrices.append(stockPrice)
-    optionPayoffs = np.array(optionPayoffs)
-    stockPrices = np.array(stockPrices)
-    newPayoffs = optionPayoffs - lambdaStar * (stockPrices - np.average(stockPrices))
-    optionPayoffs *= np.exp(-r*T)
-    newPayoffs *= np.exp(-r*T)
-    oldEstimate = np.average(optionPayoffs)
-    oldVariance = np.var(optionPayoffs)
-    newEstimate = np.average(newPayoffs)
-    estimateVariance = np.var(newPayoffs)
-    return (oldEstimate, oldVariance, newEstimate, estimateVariance)
+            totalPayoff += max(stockPrice - strikePrice, 0)
+        totalPayoff -= stockPrice - ST
+    optionPremium = totalPayoff / I / math.exp(r * T)
+    return optionPremium
